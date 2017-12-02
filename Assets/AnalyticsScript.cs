d23b50425f6388ccc2178cbc4c293a6ddaed0a0d
Charles Miller <cmmiller1111@gmail.com>
2017-12-02 13:14:26 -0500

    move project into version control

---
+++ Assets/AnalyticsScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class AnalyticsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Gender gender = Gender.Male;
+		Analytics.SetUserGender(gender);
+
+		int birthYear = 1992;
+		Analytics.SetUserBirthYear(birthYear);
+
+		LevelUp(); 
+	}
+	
+	void LevelUp() {
+		Analytics.Transaction("levelUp", 1, "EUR", null, null);
+		Analytics.CustomEvent("levelUp", new Dictionary<string, object>
+		{
+			{ "newLevel", 2 },
+			{ "trigger", "purchase" }
+		});
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
